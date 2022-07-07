1b12f1b84bca5e8fd10c6328085430f0b85c3e8e
brandonzane <bmushori@gmail.com>
2022-07-07 13:59:08 +0200

    Typings configuration, posts setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
-import type { NextPage } from "next";
 import Head from "next/head";
 import Hero from "../components/Hero";
 import Header from "../components/Header";
+import Link from "next/link";
+import { sanityClient, urlFor } from "../sanity";
+import { Post } from "../typings";
 
-const Home: NextPage = () => {
+interface Props {
+  posts: [Post];
+}
+
+const Home = ({ posts }: Props) => {
+  console.log(posts);
   return (
     <div className="">
       <Head>
@@ -15,8 +22,53 @@ const Home: NextPage = () => {
       <Hero />
 
       {/* Posts */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6">
+        {posts.map((post) => (
+          <Link key={post._id} href={`/post/${post.slug.current}`}>
+            <div className="">
+              <img src={urlFor(post.mainImage).url()!} alt="" />
+              <div className="flex justify-between p-5 bg-white">
+                <div>
+                  <p>{post.title}</p>
+                  <p>
+                    {post.description} by {post.author.name}
+                  </p>
+                </div>
+
+                <img
+                  className="h-12 w-12 rounded-full"
+                  src={urlFor(post.author.image).url()!}
+                  alt="author image"
+                />
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
 
 export default Home;
+
+export const getServerSideProps = async () => {
+  const query = `*[_type == "post"]{
+    _id,
+    title,
+    author -> {
+    name,
+    image
+  },
+   description,
+   mainImage,
+   slug
+  }`;
+
+  const posts = await sanityClient.fetch(query);
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
